26ac0f04060ca02a208d1174db989575cdf818a3
Tuong <Tuong.vu@acs-connect.com>
2016-10-26 22:58:09 -0400

    first code commit

---
+++ app/src/main/java/com/trafficcop/trafficcontrol/TrafficControlFragment.java
@@ -0,0 +1,10 @@
+package com.trafficcop.trafficcontrol;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by Thach on 10/26/16.
+ */
+
+public class TrafficControlFragment extends Fragment {
+}
